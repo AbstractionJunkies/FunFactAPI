c51c9af1960de3f118f34f5fcc058f19a7576b18
RuzmanovDev <stoyan.ruzmanov@gmail.com>
2017-01-03 00:06:12 +0200

    Changed base image url

--- server/data/facts-data.js
+++ server/data/facts-data.js
@@ -91,7 +91,7 @@ module.exports = (models) => {
             let fact = new Fact({
                 title,
                 uploader,
-                img: 'http://localhost:1337/static/images/fact-images/' + img, // TODO change before deploy
+                img: 'https://fun-fact-api.herokuapp.com/' + img, // TODO change before deploy
                 category,
                 usersRated
             });
